6141e3240c8f693ccf344ec6e3740cfa23da34c6
samiksha-chaudhari <spchaudhari80@gmail.com>
2021-09-25 14:26:55 +0530

    finding frequency of words

---
+++ Hash_Table/HashTable.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Hash_Table
+{
+    class HashTable<K, V>
+    {
+        public int size;
+        public struct KValue<k, v>
+        {
+            public k Key { get; set; }
+            public v Value { get; set; }
+        }
+
+        public LinkedList<KValue<K, V>>[] items;
+        public HashTable(int size)
+        {
+            this.size = size;
+            this.items = new LinkedList<KValue<K, V>>[size];
+
+        }
+        /// <summary>
+        /// method to get array position
+        /// </summary>
+        /// <param name="key"></param>
+        /// <returns></returns>
+        public int GetPosition(K key)
+        {
+            int position = key.GetHashCode() % size;
+            return Math.Abs(position);
+        }
+        public void Add(K Key, V value)
+        {
+            int position = GetPosition(Key);
+            LinkedList<KValue<K, V>> linkedlist = GetLinkedList(position);
+            KValue<K, V> item = new KValue<K, V>() { Key = Key, Value = value };
+            linkedlist.AddLast(item);
+        }
+        
+        public LinkedList<KValue<K, V>> GetLinkedList(int position)
+        {
+            LinkedList<KValue<K, V>> linkedList = items[position];
+            if (linkedList == null)
+            {
+                linkedList = new LinkedList<KValue<K, V>>();
+                items[position] = linkedList;
+            }
+            return linkedList;
+        }
+        
+        public V Get(K key)
+        {
+            int position = GetPosition(key);
+            LinkedList<KValue<K, V>> linkedList = GetLinkedList(position);
+            foreach (KValue<K, V> item in linkedList)
+            {
+                if (item.Key.Equals(key))
+                {
+                    return item.Value;
+                }
+            }
+            return default(V);
+        }
+
+    }
+}
--- Hash_Table/Program.cs
+++ Hash_Table/Program.cs
@@ -7,6 +7,17 @@ namespace Hash_Table
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to hash table");
+            HashTable<string, string> hash = new HashTable<string, string>(5);
+            hash.Add("0", "To");
+            hash.Add("1", "be");
+            hash.Add("2", "or");
+            hash.Add("3", "not");
+            hash.Add("4", "to");
+            hash.Add("5", "be");
+            string hash5 = hash.Get("5");
+            Console.WriteLine("5th index value : {0} ", hash5);
+            string hash2 = hash.Get("2");
+            Console.WriteLine("2nd index value : {0} ", hash2);
         }
     }
 }
